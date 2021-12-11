1bb6d57fa87757e4146b9bbdc46ccd28f7b07c6c
ksa4921234 <ksa741852963@gmail.com>
2021-12-11 14:18:31 +0800

    foxfix

--- index.js
+++ index.js
@@ -28,65 +28,67 @@ app.get('/', async (req, res) => {
     const oneminPowerPosts = await PowerPost.find({})
     var wattarray = [];
     var datearray = [];
-    for (var i = oneminPowerPosts.length - 5; i < oneminPowerPosts.length; i++) {
-        wattarray.push(oneminPowerPosts[i].功率);
-        datearray.push((oneminPowerPosts[i].createdAt.getYear() + 1900) + "-" +
-            (oneminPowerPosts[i].createdAt.getMonth() + 1) + "-" +
-            oneminPowerPosts[i].createdAt.getDate() + " " +
-            oneminPowerPosts[i].createdAt.getHours() + ":" +
-            oneminPowerPosts[i].createdAt.getMinutes() + ":" +
-            oneminPowerPosts[i].createdAt.getSeconds())
+    if (oneminPowerPosts.length > 1) {
+        for (var i = oneminPowerPosts.length - 5; i < oneminPowerPosts.length; i++) {
+            wattarray.push(oneminPowerPosts[i].功率);
+            datearray.push((oneminPowerPosts[i].createdAt.getYear() + 1900) + "-" +
+                (oneminPowerPosts[i].createdAt.getMonth() + 1) + "-" +
+                oneminPowerPosts[i].createdAt.getDate() + " " +
+                oneminPowerPosts[i].createdAt.getHours() + ":" +
+                oneminPowerPosts[i].createdAt.getMinutes() + ":" +
+                oneminPowerPosts[i].createdAt.getSeconds())
+        }
+        wattarray = JSON.stringify(wattarray);
+        datearray = JSON.stringify(datearray);
+        res.render('index', {
+            oneminPowerPosts: oneminPowerPosts,
+            wattarray: wattarray,
+            datearray: datearray
+        });
+    }else{
+        res.render('index', {
+            oneminPowerPosts: oneminPowerPosts,
+            wattarray: wattarray,
+            datearray: datearray
+        });
     }
-    wattarray = JSON.stringify(wattarray);
-    datearray = JSON.stringify(datearray);
-    res.render('index', {
-        oneminPowerPosts: oneminPowerPosts,
-        wattarray: wattarray,
-        datearray: datearray
-    });
-})
 
 
-app.get('/10min', async (req, res) => {
-    const oneminPowerPosts = await PowerPost.find({})
-    var tenminsPowerPosts = 0
-    for (var i = 0; i < oneminPowerPosts.length; i++) {
-        tenminsPowerPosts = tenminsPowerPosts + oneminPowerPosts[i].功率
-    }
-    tenminsPowerPosts = tenminsPowerPosts / oneminPowerPosts.length
-    res.render('10min', {
-        oneminPowerPosts: oneminPowerPosts,
-        tenminsPowerPosts: tenminsPowerPosts.toFixed(2)
-    });
 })
 
 var opt = {
     port: 1883,
-    clientId: 'nodejs'
+    clientId: `mqtt_${Math.random().toString(16).slice(3)}`
 };
 
 var client = mqtt.connect('mqtt://broker.emqx.io', opt);
 
 client.on('connect', function () {
     client.subscribe('/EM330/shelly/25/on');
+    client.subscribe('/EM330/shelly/25/off');
+    client.subscribe('/EM330/sensibo/on');
+    client.subscribe('/EM330/sensibo/off');
 });
 
 var http = app.listen(Port, () => {
     console.log("App listening on port 4000")
 })
 var sio = io.listen(http);
+
 sio.sockets.on('connection', function (socket) {
     socket.on('on', function (value) {
         sss = value;
         console.log(value);
         var aaa = sss.toString();
-        client.publish('/EM330/shelly/25/on', aaa)
+        // client.publish('/EM330/shelly/25/on', aaa)
+        client.publish('/EM330/sensibo/on', aaa)
     });
-    
+
     socket.on('off', function (value) {
         sss = value;
         console.log(value);
         var aaa = sss.toString();
-        client.publish('/EM330/shelly/25/off', aaa)
+        // client.publish('/EM330/shelly/25/off', aaa)
+        client.publish('/EM330/sensibo/off', aaa)
     });
 });
